7a9cac0c0f0814acf3f4e1e67a382b40e7d35b52
jbussi <joao.paulo.bussi1@gmail.com>
2025-06-04 22:33:25 -0300

    Atualização do código em grafo.py

--- src/grafo.py
+++ src/grafo.py
@@ -1,7 +1,14 @@
 #Grafo com distâncias entre cada par 
 class Grafo:
     def __init__(self):
-        self.arestas = {}
+        self.nos = {}
     
-    def adicionar_aresta(self, origem, destino, custo):
-        self.arestas
+    def adicionar_no(self, no):
+        if no not in self.nos:
+            self.nos[no] = {}
+    def adicionar_aresta(self, origem, destino, tempo: float):
+        self.adjacencias[origem][destino] = tempo
+        self.adjacencias[destino][origem] = tempo 
+    
+    def custo(self, origem, destino):
+        return self.arestas[(origem, destino)]
