6887c344d170cec272c57345a1de5adf5a32d963
jbussi <joao.paulo.bussi1@gmail.com>
2025-06-05 08:59:09 -0300

    Atualização de foco.py

--- src/foco.py
+++ src/foco.py
@@ -1,7 +1,19 @@
 #Classe foco de incêndio e suas funções
 class Foco:
-    def __init__(self, id, area_inicial, alpha):
+    def __init__(self, id: str, area_inicial: float, alpha: float):
         self.id = id
         self.area = area_inicial
         self.alpha = alpha
         self.extinto = False
+        self.data_extincao = None
+
+        self.historico_combate = {}
+
+    def registrar_combate(self, dia: int, posto_id: str, tempo: float, recursos: float):
+        if dia not in self.historico_combate:
+            self.historico_combate[dia] = {}
+        if posto_id not in self.historico_combate[dia]:
+            self.historico_combate[dia][posto_id] = {'horas': 0, 'recursos': 0}
+        
+        self.historico_combate[dia][posto_id]['horas'] += tempo
+        self.historico_combate[dia][posto_id]['recursos'] += recursos
